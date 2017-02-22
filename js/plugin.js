007afc26f3ae4898bc19d15a058ecae4475fe254
Leandro <lgalindo@ciandt.com>
2017-02-21 23:44:20 -0300

    Template hit types

--- js/plugin.js
+++ js/plugin.js
@@ -1,15 +1,18 @@
-var URL_COLLECT = '/collect';
+var URL_COLLECT = 'google-analytics.com/collect';
 var HIT_TYPE = [
+	'event',
+	'exception',
 	'pageview',
-	'event'
+	'social',
+	'timing'
 ];
-
 
 function isURLRequestGA(url) {
 	return url.indexOf(URL_COLLECT) > 0;
 } // isURLRequestGA
 
 
+// @todo Implementar verificação de eventos e pageview de enhanced e-commerce
 function hitType(url) {
 	
 	// Implementar try catch;
@@ -22,22 +25,110 @@ function hitType(url) {
 } // hitType
 
 
-function isPageview(t) {
+function isEvent(t) {
 	return HIT_TYPE[0] === t;
+} // isEvent
+
+
+function isException(t) {
+	return HIT_TYPE[1] === t;
+} // isException
+
+
+function isPageview(t) {
+	return HIT_TYPE[2] === t;
 } // isPageview
 
 
-function isEvent(t) {
-	return HIT_TYPE[1] === t;
-} // isEvent
+function isSocial(t) {
+	return HIT_TYPE[3] === t;
+} // isSocial
+
+
+function isTiming(t) {
+	return HIT_TYPE[4] === t;
+} // isTiming
+
+
+function eventTemplate(url) {
+	
+	return {
+		'hitType': HIT_TYPE[0],
+		'host': getHost(url),
+		'path': getParameterByName('dp', url),
+		'eventCategory': getParameterByName('ec', url),
+		'eventAction': getParameterByName('ea', url),
+		'eventLabel': getParameterByName('el', url),
+		'eventValue': getParameterByName('ev', url)
+	};
+	
+} // eventTemplate
+
+
+function exceptionTemplate(url) {
+	
+	return {
+		'hitType': HIT_TYPE[1],
+		'host': getHost(url),
+		'path': getParameterByName('dp', url),
+		'exceptionDescription': getParameterByName('exd', url),
+		'exceptionFatal': getParameterByName('exf', url)
+	};
+	
+} // exceptionTemplate
+
+
+function pageviewTemplate(url) {
+	
+	return {
+		'hitType': HIT_TYPE[2],
+		'host': getHost(url),
+		'path': getParameterByName('dp', url)
+	};
+	
+} // pageviewTemplate
+
+
+function socialTemplate(url) {
+	
+	return {
+		'hitType': HIT_TYPE[3],
+		'host': getHost(url),
+		'path': getParameterByName('dp', url),
+		'socialNetworking': getParameterByName('sn', url),
+		'socialAction': getParameterByName('sa', url),
+		'socialTarget': getParameterByName('st', url)
+	};
+	
+} // socialTemplate
+
+
+function timingTemplate(url) {
+	
+	return {
+		'hitType': HIT_TYPE[4],
+		'host': getHost(url),
+		'path': getParameterByName('dp', url),
+		'timingCategory': getParameterByName('utc', url),
+		'timingVariable': getParameterByName('utv', url),
+		'timingTime': getParameterByName('utt', url),
+		'timingLabel': getParameterByName('utl', url)
+	};
+	
+} // timingTemplate
+
+
+function getHost(url) {
+	return (getParameterByName('dr', url)) ? getParameterByName('dr', url) : getParameterByName('dl', url);
+} // getHost
 
 
 function getParameterByName(name, url) {
 	
     name = name.replace(/[\[\]]/g, "\\$&");
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
+        results = regex.exec(url.toLowerCase());
 		
-    return (!results || !results[2]) ? false : decodeURIComponent(results[2].replace(/\+/g, " "));
+    return (!results || !results[2]) ? false : decodeURIComponent(results[2].replace(/\+/g, " ")).toLowerCase();
 	
 } // getParameterByName
